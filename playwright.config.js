e643abc12d1c4c161309f09b47756b4fcf479ea2
Talisson <talissonwerley.dev@gmail.com>
2025-08-06 22:02:33 -0300

    Adicionado README.MD, E

--- playwright.config.js
+++ playwright.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     ['html', { outputFolder: 'playwright-report', open: 'never' }],
   ],
   use: {
-    headless: false,
+    headless: true,
     viewport: { width: 1280, height: 720 },
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
